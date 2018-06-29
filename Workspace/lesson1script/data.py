2af28ff6e7302b56935235df3845db30360af3a5
Sophia Eno <sopeno@nuevaschool.org>
2018-06-29 14:49:46 -0700

    final project

---
+++ Workspace/lesson1script/data.py
@@ -0,0 +1,11 @@
+grid = [["SFFF"], ["FFFG"], ["ABFF"], ["GFFF"]]
+for c in grid:
+    row = ""
+    for r in c:
+        row +=r
+        print r
+
+#myEmptyList = []
+#myListWithStuff = ["thing", 1, "thing", 2]
+#print (myListWithStuff[3])
+#['F', 'F', 'F', 'G'], ['A', 'B', 'F', 'F'], ['C', 'G', 'F', 'G']
